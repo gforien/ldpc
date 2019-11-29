90dc93e423f4689901d1e4cf25338c18d536979d
Gabriel Forien <gforien@gmail.com>
2019-11-29 17:22:55 +0100

    first commit :tada:

---
+++ HARD_DECODER_GROUPE_6.m
@@ -0,0 +1,7 @@
+function c_cor = HARD_DECODER_GROUPE_6(c, H, MAX_ITER)
+%LDPC on sait pas trop ce que ça fait
+%   c : vecteur colonne de taille N
+%   H : matrice de taille M, N
+
+    c_cor = 1
+end
